df5c1cd8068e55e611e95b9d80a38bb0c8e9b64e
adilhan <adilhancetin@otsimo.com>
2024-08-23 10:13:30 +0300

    Update request.py

--- requests/request.py
+++ requests/request.py
@@ -5,11 +5,28 @@ client = openai.OpenAI(
     api_key = os.getenv("api_key")
 )
 
+prompt = """
+Give me 10 questions about the history of taekwondo in the following format:
+1) <question>
+a) <option-1>
+b) <option-2>
+c) <option-3>
+d) <option-4>
+2) <question>
+a) <option-1>
+b) <option-2>
+c) <option-3>
+d) <option-4>
+...
+At the end, provide the correct answers in this format:
+answers: abcdabcdab
+"""
+
 chat_completion = client.chat.completions.create(
     messages=[
         {
             "role": "user",
-            "content": "Give me 10 questions about history of Turkey, the format will be like: 1)<question> a)<> ... . At the end give then answers like answers:abcdabcdab",
+            "content": prompt,
         }
     ],
     model="gpt-4o",
